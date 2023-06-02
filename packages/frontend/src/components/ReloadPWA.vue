0fba899d37a8f2491967f4471e15604a32f478ec
Notaovercat <fdgoe@list.ru>
2023-06-02 23:12:19 +1000

    fixed pwa import

--- packages/frontend/src/components/ReloadPWA.vue
+++ packages/frontend/src/components/ReloadPWA.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-// @ts-ignore
-import { useRegisterSW } from "virtual:pwa-register";
+import { useRegisterSW } from "virtual:pwa-register/vue";
 
 const { offlineReady, needRefresh } = useRegisterSW();
 
