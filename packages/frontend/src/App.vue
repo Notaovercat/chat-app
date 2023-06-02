84e1b3830c7937d909030889be3c5497449aeaee
Notaovercat <fdgoe@list.ru>
2023-06-02 22:46:58 +1000

    added pwa

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import ReloadPWA from "./components/ReloadPWA.vue";
 </script>
 
 <template>
+  <ReloadPWA />
   <RouterView />
 </template>
--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,14 +1,61 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+const pwaConfig = VitePWA({
+  registerType: "autoUpdate",
+  base: "/",
+  srcDir: "src",
+  filename: "sw.ts",
+  outDir: "dist",
+  strategies: "injectManifest",
+  manifest: {
+    name: "chat-app",
+    short_name: "chat",
+    description: "Chat app maid with Vue.js 3 and Typescript",
+    start_url: "/",
+    display: "standalone",
+    theme_color: "white",
+    icons: [
+      {
+        src: "icons/manifest-icon-192.maskable.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "icons/manifest-icon-192.maskable.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "icons/manifest-icon-512.maskable.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "icons/manifest-icon-512.maskable.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+  },
+  devOptions: {
+    enabled: true,
+    type: "module",
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), pwaConfig],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
