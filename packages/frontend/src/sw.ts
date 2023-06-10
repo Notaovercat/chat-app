397d253044e302317b6b94fc573469861fd1e69d
Notaovercat <fdgoe@list.ru>
2023-06-10 12:37:52 +1000

    fixed caching

--- packages/frontend/src/sw.ts
+++ packages/frontend/src/sw.ts
@@ -2,81 +2,50 @@ import { precacheAndRoute } from "workbox-precaching";
 
 declare const self: ServiceWorkerGlobalScope;
 
+precacheAndRoute(self.__WB_MANIFEST);
+
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") self.skipWaiting();
 });
 
-self.addEventListener("install", (event) => {
-  console.log("Service Worker installed");
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-  console.log("Service Worker activated");
-});
-
-// self.addEventListener("fetch", (event) => {
-//   event.respondWith(
-//     (async function () {
-//       const cacheName = "api-cache";
-//       const cache = await caches.open(cacheName);
-//       const cachedResponse = await cache.match(event.request);
-
-//       const fetchPromise = fetch(event.request);
-
-//       event.waitUntil(
-//         (async function () {
-//           const networkResponse = await fetchPromise;
-//           const cacheControlHeader =
-//             networkResponse.headers.get("Cache-Control");
-
-//           if (cacheControlHeader && cacheControlHeader.includes("no-cache")) {
-//             // If the response indicates no-cache, delete the existing cache entry
-//             await cache.delete(event.request);
-//           } else {
-//             // Otherwise, update the cache with the new response
-//             await cache.put(event.request, networkResponse.clone());
-//           }
-//         })()
-//       );
-
-//       // Return the cached response if available, otherwise return the network response.
-//       return cachedResponse || fetchPromise;
-//     })()
-//   );
-// });
-
+/*
 self.addEventListener("fetch", (event) => {
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      fetch(event.request).catch(async () => {
-        const cache = await caches.open("api-cache");
-        const cachedResponse = await cache.match("/offline.html");
-        return (
-          cachedResponse ||
-          new Response("Offline", { status: 503, statusText: "Offline" })
-        );
+  event.respondWith(
+    fetch(event.request)
+    .then((response) => {
+        // If we get a response from the network, update the cache.
+        caches
+        .open("api-cache")
+        .then((cache) => cache.put(event.request, response.clone()));
+        return response;
       })
-    );
-  } else {
-    event.respondWith(
-      (async function () {
-        const cache = await caches.open("api-cache");
-        const cachedResponse = await cache.match(event.request);
-        const networkResponsePromise = fetch(event.request);
+      .catch(async () => {
+        // If the network request fails, try to return a response from the cache.
+        const cacheResponse = await caches.match(event.request);
+        return cacheResponse || new Response("No response found in cache.");
+      })
+      );
+    });
+    */
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // Clone the response right after it's received
+        const clonedResponse = response.clone();
+        // Use the cloned response to update the cache
+        caches
+          .open("api-cache")
+          .then((cache) => cache.put(event.request, clonedResponse));
 
-        event.waitUntil(
-          (async function () {
-            const networkResponse = await networkResponsePromise;
-            await cache.put(event.request, networkResponse.clone());
-          })()
-        );
-
-        // Returned the cached response if we have one, otherwise return the network response.
-        return cachedResponse || networkResponsePromise;
-      })()
-    );
-  }
+        // Return the original response to the fetch event
+        return response;
+      })
+      .catch(() => {
+        // If the network request fails, try to return a response from the cache.
+        return caches.match(event.request).then((cacheResponse) => {
+          return cacheResponse || new Response("No response found in cache.");
+        });
+      })
+  );
 });
-
-precacheAndRoute(self.__WB_MANIFEST);
