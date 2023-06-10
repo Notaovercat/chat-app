c5a0b7fe48a0f566a9f7e7de840c0634c824ca27
Notaovercat <fdgoe@list.ru>
2023-06-11 01:57:11 +1000

    added push register

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -1,6 +1,43 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import ReloadPWA from "@/components/additional/ReloadPWA.vue";
+import { onMounted } from "vue";
+
+function urlBase64ToUint8Array(base64String: string) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+onMounted(() => {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.ready.then(function (registration) {
+      registration.pushManager
+        .subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: urlBase64ToUint8Array(
+            import.meta.env.VITE_VAPID_PUBLIC_KEY as string
+          ),
+        })
+        .then(function (subscription) {
+          // The subscription was successful
+          console.log("User is subscribed:", subscription);
+        })
+        .catch(function (err) {
+          console.log("Failed to subscribe the user: ", err);
+        });
+    });
+  }
+});
 </script>
 
 <template>
--- packages/frontend/src/sw.ts
+++ packages/frontend/src/sw.ts
@@ -9,21 +9,21 @@ self.addEventListener("message", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
   event.respondWith(
     fetch(event.request)
       .then((response) => {
-        // Clone the response right after it's received
         const clonedResponse = response.clone();
-        // Use the cloned response to update the cache
         caches
           .open("api-cache")
           .then((cache) => cache.put(event.request, clonedResponse));
-
-        // Return the original response to the fetch event
         return response;
       })
       .catch(() => {
-        // If the network request fails, try to return a response from the cache.
         return caches.match(event.request).then((cacheResponse) => {
           return cacheResponse || new Response("No response found in cache.");
         });
